9dcd4dc807752b9f56d228e25ec8f1b3f6de5aaa
Riku Oja <riku.oja@kapsi.fi>
2022-01-18 16:23:33 +0200

    Add service code layers to map

--- import_open311.py
+++ import_open311.py
@@ -1,9 +1,33 @@
+import csv
 import datetime
 import json
 import requests
 from time import sleep
 
 OPEN311_BASE_URL = "https://asiointi.hel.fi/palautews/rest/v1/requests.json"
+
+
+def import_service_codes(file: str = "data/PATE_active_categories.csv") -> dict:
+    categories = {}
+    with open(file) as csv_file:
+        reader = csv.reader(csv_file, delimiter=";")
+        header = next(reader)
+        if (
+            not header[0] == "ID"
+            or not header[1] == "NAME"
+            or not header[2] == "ORGANIZATION"
+            or not header[3] == "PARENT_ID"
+        ):
+            raise AssertionError(
+                "The csv file must have fields ID, NAME, ORGANIZATION, PARENT_ID in that order"
+            )
+        for row in reader:
+            categories[row[0]] = {
+                "name": row[1],
+                "organization": row[2],
+                "parent": row[3],
+            }
+    return categories
 
 
 def import_data(years: int = 2) -> dict:
